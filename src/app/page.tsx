802f46bbc9f1536befc2217dd597b6811baeeaab
webw1zard <ulugbeknizomov999@gmail.com>
2025-02-08 16:44:38 +0500

    h-881

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  "https://jyighudlgmskjajoquce.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5aWdodWRsZ21za2pham9xdWNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4MDY5NDEsImV4cCI6MjA1MzM4Mjk0MX0.MX9NDGh7taEIiIihoAgQS5MMDwwd8Q8cMjqX5qo0inc"
-);
+import { createClient } from "../supabase/client";
 
 interface User {
   id: number;
@@ -24,6 +19,7 @@ export default function Home() {
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const [isEditing, setIsEditing] = useState(false);
 
+  const supabase = createClient();
   useEffect(() => {
     fetchUsers();
   }, []);
